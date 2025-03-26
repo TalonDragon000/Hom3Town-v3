5d7edb1474876edd1ec3d064dee1c00240ae733f
TalonDragon000 <talondragon000@gmail.com>
2025-03-25 21:08:06 -1000

    updated anon.png & size

--- account-kit-app/app/create-avatar/page.tsx
+++ account-kit-app/app/create-avatar/page.tsx
@@ -5,7 +5,7 @@ import CharacterCreatorClient from "../components/CharacterCreatorClient";
 import { Avatar } from "../components/Avatar";
 
 export default function CreateAvatar() {
-    const defaultImagePath = "/images/annon.png";
+    const defaultImagePath = "/images/anon.png";
 
     return (
       <main className="flex mt-10 flex-col items-center justify-center p-12">
@@ -13,12 +13,13 @@ export default function CreateAvatar() {
           <div className="nav-item-left font-bold"><a href="/">Hom3Town</a></div>
           <div className="nav-item-right mt-2">
             <Avatar 
-              defaultImagePath={"/images/annon.png"} 
+              defaultImagePath={"/images/anon.png"} 
               size="small"
             />
           </div>
           <div className="nav-login"><Login /></div>
         </nav>
+
         <div className="text-center mt-5 p-4">
             <h2 className="text-center text-5xl font-bold mb-5">Create your avatar</h2>
         </div>
